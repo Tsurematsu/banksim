45febc3bba7fe9f964d5d7bfe2174dd943519bae
José Daniel Solano Gracia <daniel120802@gmail.com>
2025-11-20 19:21:26 -0500

    update icons

--- src/LoginPage/LoginPage.tsx
+++ src/LoginPage/LoginPage.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import assets from '../assets';
-
+import img from '../assets/img';
 export default function LoginPage() {
   const navigate = useNavigate();
   // Estado para manejar la visibilidad de la contraseña
---
+++ src/assets/img.ts
@@ -0,0 +1,9 @@
+/* eslint-disable */
+// ⚠️ ARCHIVO GENERADO AUTOMÁTICAMENTE
+// Las rutas son absolutas respecto al servidor (comienzan con /)
+
+const img = {
+  "account_balance": "/account_balance.svg",
+  "vite": "/vite.svg"
+};
+export default img
